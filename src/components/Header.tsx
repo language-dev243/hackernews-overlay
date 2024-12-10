d67ee2de949f4cfcb8defca4bb9280b11d7c76d4
language-dev243 <language-dev@protonmail.com>
2024-12-10 15:50:14 +0100

    updating heading to hackernews-overlay

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import HeaderTitle from "./HeaderTitle";
 const Header: React.FC = () => {
   return (
     <header className="bg-gray-900 py-4 text-center">
-      <HeaderTitle text="hacker news" />
+      <HeaderTitle text="hackernews-overlay^" />
     </header>
   );
 };
